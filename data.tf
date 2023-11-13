b24bce771f2185989a2a0a4ec14e050ddd0655e9
Stefan Freitag <stefan.freitag@rwe.com>
2023-11-13 20:59:13 +0100

    feat: - Add "output_file_mode" option with a value of "0666"

--- data.tf
+++ data.tf
@@ -6,7 +6,9 @@ data "aws_region" "current" {}
 
 # Creates the zip file for the Lambda function
 data "archive_file" "status_checker_code" {
-  type        = "zip"
-  source_dir  = "${path.module}/functions/check-fsx-status/"
-  output_path = "${path.module}/out/check-fsx-status.zip"
+  type                        = "zip"
+  source_dir                  = "${path.module}/functions/check-fsx-status/"
+  output_path                 = "${path.module}/out/check-fsx-status.zip"
+  exclude_symlink_directories = false
+  output_file_mode            = "0666"
 }
